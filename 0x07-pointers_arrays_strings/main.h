a31e302172a272ebc829b79b61c365bfdfc2184d
peter <peterkamil71@gmail.com>
2023-08-01 02:59:40 -0700

    h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,11 +4,11 @@
 int _putchar(char c);
 char *_memset(char *c, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-cgar *_strchr(char *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[0]);
+void print_chessboard(char (*a));
 void print_diagrams(int *a, int size);
 void set_string(char **s, char *to);
 
